a50af1505d72774d24b29dd6efbd6d22caf72999
Your Name <zc_program@163.com>
2019-07-04 16:21:04 +0800

    修改jni相关内容

--- Zcrop-Library/src/main/jni/me_pandazhang_app_zcrop_BitmapCropTask.h
+++ Zcrop-Library/src/main/jni/me_pandazhang_app_zcrop_BitmapCropTask.h
@@ -2,8 +2,8 @@
 #include <jni.h>
 /* Header for class com_yalantis_ucrop_task_BitmapCropTask */
 
-#ifndef _Included_com_yalantis_ucrop_task_BitmapCropTask
-#define _Included_com_yalantis_ucrop_task_BitmapCropTask
+#ifndef _Included_me_pandazhang_app_zcrop_task_BitmapCropTask
+#define _Included_me_pandazhang_app_zcrop_task_BitmapCropTask
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,7 +13,7 @@ extern "C" {
 * Method:    cropCImg
 * Signature: (Ljava/lang/String;Ljava/lang/String;IIIIF)Z
 */
-JNIEXPORT jboolean JNICALL Java_com_yalantis_ucrop_task_BitmapCropTask_cropCImg
+JNIEXPORT jboolean JNICALL Java_cme_pandazhang_app_zcrop_task_BitmapCropTask_cropCImg
 (JNIEnv *, jobject, jstring, jstring, jint, jint, jint, jint, jfloat, jfloat, jint, jint, jint, jint);
 
 #ifdef __cplusplus
